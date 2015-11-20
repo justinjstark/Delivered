fa6ae5d3b4f59e0c22a816d53774cc9c7ef6341f
justinjstark <fromgithub@justinjstark.com>
2015-11-20 09:39:03 -0600

    Move DeliveryService abstract class to own file.

---
+++ Distributor/DeliveryService.cs
@@ -0,0 +1,24 @@
+﻿namespace Distributor
+{
+    public abstract class DeliveryService<TEndpoint> : IDeliveryService where TEndpoint : IEndpoint
+    {
+        protected abstract void DeliverFileToEndpoint(DistributionFile file, TEndpoint endpoint);
+
+        private readonly IEndpointRepository<TEndpoint> _endpointRepository;
+
+        protected DeliveryService(IEndpointRepository<TEndpoint> endpointRepository)
+        {
+            _endpointRepository = endpointRepository;
+        }
+
+        public void DeliverFile(DistributionFile file)
+        {
+            var endpoints = _endpointRepository.GetEndpointsForProfile(file.ProfileName);
+
+            foreach (var endpoint in endpoints)
+            {
+                DeliverFileToEndpoint(file, endpoint);
+            }
+        }
+    }
+}
--- Distributor/IDeliveryService.cs
+++ Distributor/IDeliveryService.cs
@@ -1,33 +1,7 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Security.Cryptography.X509Certificates;
-namespace Distributor
+﻿namespace Distributor
 {
     public interface IDeliveryService
     {
         void DeliverFile(DistributionFile file);
-    }
-
-    public abstract class DeliveryService<TEndpoint> : IDeliveryService where TEndpoint : IEndpoint
-    {
-        protected abstract void DeliverFileToEndpoint(DistributionFile file, TEndpoint endpoint);
-
-        private readonly IEndpointRepository<TEndpoint> _endpointRepository;
-
-        protected DeliveryService(IEndpointRepository<TEndpoint> endpointRepository)
-        {
-            _endpointRepository = endpointRepository;
-        }
-
-        public void DeliverFile(DistributionFile file)
-        {
-            var endpoints = _endpointRepository.GetEndpointsForProfile(file.ProfileName);
-
-            foreach (var endpoint in endpoints)
-            {
-                DeliverFileToEndpoint(file, endpoint);
-            }
-        }
     }
 }
